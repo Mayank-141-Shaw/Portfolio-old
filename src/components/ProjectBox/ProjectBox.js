e903209b9668854ed76b60fa722bc7280d1591a4
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-07-11 17:42:16 +0530

    Changed the backgorund of waves

--- src/components/ProjectBox/ProjectBox.js
+++ src/components/ProjectBox/ProjectBox.js
@@ -12,7 +12,12 @@ function ProjectBox(props) {
 
   return (
     <>
-        <div className='project-card' style={{aspectRatio:'1', backgroundColor:props.color}}
+        <div className='project-card' style={{
+            aspectRatio:'1', 
+            backgroundImage:`url(${props.img})`,
+            backgroundSize:'cover',
+            backgroundPosition:'center'
+        }}
                 onMouseEnter={toggleHoverViewButton} onMouseLeave={toggleHoverViewButton}>
             <div className={`hover-round ${toggleViewProjectBtn ? 'show-hover' : 'hide-hover'}`} >
                 View Project
