b7bbd3e3d2dd6587c35d7c89003c4aaa2796e9d9
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-07-18 23:02:21 +0530

    Added more data

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,24 +6,72 @@ import './css/Projects.css'
 import bigImg from '../assets/images/chill.jpg'
 import ProjectDetailBox from '../components/ProjectDetailBox/ProjectDetailBox'
 
-const data = {
-  dir: "right",
-  img: bigImg,
-  title: "Hello World",
-  badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
-  desc: "theis is the description of the body of the project",
-  link: 'https://github.com/Mayank-141-Shaw/Portfolio'
-}
-
-const data2 = {
-  dir: "left",
-  img: bigImg,
-  title: "Hello World",
-  badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
-  desc: "theis is the description of the body of the project",
-  link: 'https://github.com/joeldesante/Harvey/blob/master/src/lib/courseChannels.js'
-}
-
+const data = [
+  {
+    dir: "right",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/Mayank-141-Shaw/Portfolio'
+  },
+  {
+    dir: "left",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/joeldesante/Harvey/blob/master/src/lib/courseChannels.js'
+  },
+  {
+    dir: "right",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/Mayank-141-Shaw/Portfolio'
+  },
+  {
+    dir: "left",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/joeldesante/Harvey/blob/master/src/lib/courseChannels.js'
+  },
+  {
+    dir: "right",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/Mayank-141-Shaw/Portfolio'
+  },
+  {
+    dir: "left",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/joeldesante/Harvey/blob/master/src/lib/courseChannels.js'
+  },
+  {
+    dir: "right",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/Mayank-141-Shaw/Portfolio'
+  },
+  {
+    dir: "left",
+    img: bigImg,
+    title: "Hello World",
+    badges: ['HTML', 'Sql', 'JavaScript', 'CSS', 'Spring Boot', 'Three Js', 'Bootstrap', 'Tensorflow', 'Python', 'Ionic'],
+    desc: "theis is the description of the body of the project",
+    link: 'https://github.com/joeldesante/Harvey/blob/master/src/lib/courseChannels.js'
+  },
+]
 
 
 function Projects() {
@@ -43,12 +91,17 @@ function Projects() {
               className='year-end'>
               2022
             </Badge>
-            <hr style={{ display: 'block', width: '100%', backgroundColor:'#ffffff66' }} />
+            <hr style={{ display: 'block', width: '100%', backgroundColor: '#ffffff66' }} />
           </div>
 
-          <ProjectDetailBox info={data} />
-          <ProjectDetailBox info={data2} />
-
+          <ProjectDetailBox info={data[0]} />
+          <ProjectDetailBox info={data[1]} />
+          <ProjectDetailBox info={data[2]} />
+          <ProjectDetailBox info={data[3]} />
+          <ProjectDetailBox info={data[4]} />
+          <ProjectDetailBox info={data[5]} />
+          <ProjectDetailBox info={data[6]} />
+          <ProjectDetailBox info={data[7]} />
 
         </Container>
 
