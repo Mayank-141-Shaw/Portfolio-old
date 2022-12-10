b3220b65b362e9da20956cc958a8ff2ac7d5e37f
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-12-10 15:52:39 +0530

    Done with Text Shpere of skills

---
+++ src/components/TextSphere/TextSphere.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react'
+import "./TextSphere.css"
+
+import TagCloud from 'TagCloud'
+
+import { SkillData } from '../SkillContainer/SkillData.js'
+
+function TextSphere() {
+
+    const ref = useRef(null)
+
+    useEffect( () => {
+        return () => {
+
+            const container = ".tagcloud";
+            const texts =  [...Object.values(SkillData).flat().map( skill => skill.title )]
+            const options = {
+                radius: (ref.current.parentElement.offsetWidth - 100)/2,
+
+                maxSpeed: "normal",
+                initSpeed: "normal",
+                keep: true,
+            };
+
+            TagCloud(container, texts, options)
+        };
+    }, [])
+
+  return (
+    <>
+        <div ref={ref} className='text-sphere'>
+            <span className='tagcloud'></span>
+        </div>
+    </>
+  );
+}
+
+export default TextSphere
