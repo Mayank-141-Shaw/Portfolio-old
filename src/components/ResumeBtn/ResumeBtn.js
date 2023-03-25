1038c1a50558325af52aab851b380b89db56327f
Mayank-141-Shaw <mayank141shaw@gmail.com>
2023-03-26 01:01:03 +0530

    Lots of icon and design changes

--- src/components/ResumeBtn/ResumeBtn.js
+++ src/components/ResumeBtn/ResumeBtn.js
@@ -1,29 +1,30 @@
-import React from 'react'
-import { OverlayTrigger, Tooltip } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { FaDownload } from 'react-icons/fa'
+import React from "react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaDownload } from "react-icons/fa";
 
-import './ResumeBtn.css'
+import "./ResumeBtn.css";
 
 function ResumeBtn() {
-
-    const renderTooltip = (props) => (
-        <Tooltip id='resume-tooltip' {...props}>
-            Click to download resume
-        </Tooltip>
-    )
+  const renderTooltip = (props) => (
+    <Tooltip id="resume-tooltip" {...props}>
+      Click to download resume
+    </Tooltip>
+  );
 
   return (
     <OverlayTrigger
-        placement='top'
-        delay={{show:250, hide:400}}
-        overlay={renderTooltip}
+      placement="top"
+      delay={{ show: 150, hide: 400 }}
+      overlay={renderTooltip}
     >
-        <div className='resume'>
-            <a href='/resume.pdf' download="Mayank-Resume"><FaDownload /> Resume</a>
-        </div>
-    </OverlayTrigger>  
-  )
+      <div className="resume">
+        <a href="/resume.pdf" download="Mayank-Resume">
+          <FaDownload /> Resume
+        </a>
+      </div>
+    </OverlayTrigger>
+  );
 }
 
-export default ResumeBtn
+export default ResumeBtn;
